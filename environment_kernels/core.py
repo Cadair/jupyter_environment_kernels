48dc331534882cfe1114bcdd3e911091f9df5d13
Stuart Mumford <stuart@cadair.com>
2015-11-05 11:53:56 +0000

    Fix native kernel bug, and add an ignored envs option

--- environment_kernels/core.py
+++ environment_kernels/core.py
@@ -3,30 +3,34 @@
 import os
 import glob
 
-from jupyter_client.kernelspec import KernelSpecManager, KernelSpec, NATIVE_KERNEL_NAME, NoSuchKernel
+from jupyter_client.kernelspec import KernelSpecManager, KernelSpec, NoSuchKernel
 from traitlets import List
 
 __all__ = ['EnvironmentKernelSpecManager']
 
+
 class EnvironmentKernelSpecManager(KernelSpecManager):
 
     env_dirs = List(['~/.conda/envs/', '~/.virtualenvs'], config=True)
-    
+    ignore_envs = List([], config=True)
+
     def _get_env_paths(self):
-        return [os.path.join(os.path.expanduser(base_dir), '*/bin/ipython') for base_dir in self.env_dirs]
+        return [os.path.join(os.path.expanduser(base_dir), '*/bin/ipython')
+                for base_dir in self.env_dirs]
 
     def find_python_paths(self):
         # find a python executeable
         python_dirs = {}
-        
+
         for env_path in self._get_env_paths():
             for python_exe in glob.glob(env_path):
                 venv_dir = os.path.split(os.path.split(python_exe)[0])[0]
                 venv_name = os.path.split(venv_dir)[1]
-                python_dirs.update({venv_name: venv_dir})
-        
+                if venv_name not in self.ignore_envs:
+                    python_dirs.update({venv_name: venv_dir})
+
         return python_dirs
-    
+
     def venv_kernel_specs(self):
         python_dirs = self.find_python_paths()
         kspecs = {}
@@ -42,21 +46,21 @@ class EnvironmentKernelSpecManager(KernelSpecManager):
 
             kspecs.update({venv_name: KernelSpec(**kspec_dict)})
         return kspecs
-    
+
     def find_kernel_specs(self):
         """Returns a dict mapping kernel names to resource directories."""
         d = super(EnvironmentKernelSpecManager, self).find_kernel_specs()
-        
+
         d.update(self.find_python_paths())
         return d
 
     def get_kernel_spec(self, kernel_name):
         """Returns a :class:`KernelSpec` instance for the given kernel_name.
-        
+
         Raises :exc:`NoSuchKernel` if the given kernel name is not found.
         """
         try:
-            super(EnvironmentKernelSpecManager, self).get_kernel_spec(kernel_name)
+            return super(EnvironmentKernelSpecManager, self).get_kernel_spec(kernel_name)
         except (NoSuchKernel, FileNotFoundError):
             if kernel_name.lower() in self.venv_kernel_specs():
                 return self.venv_kernel_specs()[kernel_name.lower()]
