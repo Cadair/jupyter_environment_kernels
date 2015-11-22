c2eab654e1b085c474bfd916f26f9cd05b8ed058
Stuart Mumford <stuart@cadair.com>
2015-11-22 17:17:43 +0000

    Add a whitelist and add a search for CONDA_ENVS_DIR.

--- environment_kernels/core.py
+++ environment_kernels/core.py
@@ -10,9 +10,43 @@ __all__ = ['EnvironmentKernelSpecManager']
 
 
 class EnvironmentKernelSpecManager(KernelSpecManager):
+    """
+    A Jupyter Kenel manager which dyamically checks for Environments
 
-    env_dirs = List(['~/.conda/envs/', '~/.virtualenvs'], config=True)
-    ignore_envs = List([], config=True)
+    Given a list of base directories, this class searches for the pattern::
+
+        BASE_DIR/NAME/bin/ipython
+
+    where NAME is taken to be the name of the environment.
+    """
+
+    # Take the default home DIR for conda and virtualenv as the default
+    _default_dirs = ['~/.conda/envs/', '~/.virtualenvs']
+
+    # Check for the windows specific CONDA_ENVS_PATH variable and add it to the
+    # list if set.
+    if os.environ.get('CONDA_ENVS_PATH', False):
+        _default_dirs.append(os.environ['CONDA_ENVS_PATH'])
+
+    env_dirs = List(_default_dirs, config=True)
+    blacklist_envs = List([], config=True)
+    whitelist_envs = List([], config=True)
+
+    def validate_env(self, envname):
+        """
+        Check the name of the environment against the black list and the
+        whitelist. If a whitelist is specified only it is checked.
+        """
+
+        if self.whitelist_envs and envname in self.whitelist_envs:
+            return True
+        elif self.whitelist_envs:
+            return False
+
+        if self.blacklist_envs and envname not in self.blacklist_envs:
+            return True
+        else:
+            return False
 
     def _get_env_paths(self):
         return [os.path.join(os.path.expanduser(base_dir), '*/bin/ipython')
@@ -26,7 +60,7 @@ class EnvironmentKernelSpecManager(KernelSpecManager):
             for python_exe in glob.glob(env_path):
                 venv_dir = os.path.split(os.path.split(python_exe)[0])[0]
                 venv_name = os.path.split(venv_dir)[1]
-                if venv_name not in self.ignore_envs:
+                if self.validate_env(venv_name):
                     python_dirs.update({venv_name: venv_dir})
 
         return python_dirs
