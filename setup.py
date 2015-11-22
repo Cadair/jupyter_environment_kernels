63e44efaf07cd1641bac40f3ac458b673eee0ec8
Stuart Mumford <stuart@cadair.com>
2015-11-22 17:17:32 +0000

    Version 1.0

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name = "environment_kernels",
     license = "BSD",
     packages=['environment_kernels'],
+    version=1.0,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
